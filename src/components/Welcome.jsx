8c3875394484a3e7ac2013272048f827f10968cb
Marcellyz <marcelly.eduarda4545@gmail.com>
2024-07-15 01:16:11 -0300

    feat: criando o contexto do quiz

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,8 +1,14 @@
-import Quiz from '../img/quiz.svg'
+import Quiz from '../img/quiz.svg';
+import { useContext } from 'react';
+import { QuizContext } from '../context/quiz';
 
 import './css/Welcome.css'
 
 const Welcome = () => {
+  const quizState = useContext(QuizContext);
+
+  console.log(quizState);
+
   return (
     <div id="welcome">
         <h2>Seja bem-vindo</h2>
