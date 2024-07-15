82e252afbc92184f7c15bacd3fea0a3a46353246
Marcellyz <marcelly.eduarda4545@gmail.com>
2024-07-15 18:42:54 -0300

    feat: progredindo perguntas do quiz

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -13,7 +13,7 @@ const Question = () => {
         <div id="options-container">
             <p>Opções</p>
         </div>
-        <button>Continuar</button>
+        <button onClick={() => dispatch({type: "CHANGE_QUESTION"})}>Continuar</button>
     </div>
   )
 }
