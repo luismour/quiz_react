b9fee986bf19e2b1f443cf9ed5f372574d8520e5
Marcellyz <marcelly.eduarda4545@gmail.com>
2024-07-15 01:31:47 -0300

    feat: configurando o reducer

--- src/context/quiz.jsx
+++ src/context/quiz.jsx
@@ -1,9 +1,31 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import questions from '../data/questions';
+
+const STAGES = ["Start", "Playing", "End"];
+
+const initialStage = {
+    gameStage: STAGES[0],
+    questions
+}
+
+const quizReducer = (state, action) => {
+
+    console.log(state, action);
+
+    switch (action.type) {
+        case "CHANGE_STATE":
+            
+            break;
+    
+        default:
+            break;
+    }
+}
 
 export const QuizContext = createContext();
 
 export const QuizProvider = ({ children }) => {
-    const value = {name: "Quiz"};
+    const value = useReducer(quizReducer, initialStage);
 
     return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>
 };
