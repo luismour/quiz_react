58d330225bc371eed9a235cb92ed4642926d16e5
Marcellyz <marcelly.eduarda4545@gmail.com>
2024-07-17 13:41:17 -0300

    fix: ajustes nas cores dos botões das opções

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -6,9 +6,19 @@ import "./css/Option.css";
 const Option = ({ option, selectOption, answer }) => {
   const [quizState, dispatch] = useContext(QuizContext);
 
-  return <div className="option" onClick={() => selectOption()}>
-    <p>{option}</p>
-  </div>;
+  return (
+    <div
+      className={`option ${
+        quizState.answerSelected && option === answer ? "correct" : ""
+      }
+      ${
+        quizState.answerSelected && option !== answer ? "wrong" : ""
+      }`}
+      onClick={() => selectOption()}
+    >
+      <p>{option}</p>
+    </div>
+  );
 };
 
 export default Option;
