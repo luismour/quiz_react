c8c63162c05157b490cd682305f369a7a73ba9b5
Marcellyz <marcelly.eduarda4545@gmail.com>
2024-07-17 12:51:17 -0300

    feat: exibindo as opções das perguntas

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,21 +1,28 @@
 import { useContext } from "react";
 import { QuizContext } from "../context/quiz";
+import Option from "./Option";
 
-import './css/Question.css';
+import "./css/Question.css";
 
 const Question = () => {
-    const [quizState, dispatch] = useContext(QuizContext);
-    const currentQuestion = quizState.questions[quizState.currentQuestion];
+  const [quizState, dispatch] = useContext(QuizContext);
+  const currentQuestion = quizState.questions[quizState.currentQuestion];
   return (
     <div id="questions">
-        <p>Pergunta {quizState.currentQuestion + 1} a {quizState.questions.length}</p>
-        <h2>{currentQuestion.question}</h2>
-        <div id="options-container">
-            <p>Opções</p>
-        </div>
-        <button onClick={() => dispatch({type: "CHANGE_QUESTION"})}>Continuar</button>
+      <p>
+        Pergunta {quizState.currentQuestion + 1} a {quizState.questions.length}
+      </p>
+      <h2>{currentQuestion.question}</h2>
+      <div id="options-container">
+        {currentQuestion.options.map((option) => (
+          <Option option={option} key={option} />
+        ))}
+      </div>
+      <button onClick={() => dispatch({ type: "CHANGE_QUESTION" })}>
+        Continuar
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Question
+export default Question;
